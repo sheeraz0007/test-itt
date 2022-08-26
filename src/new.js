abe1e748958c95ba354d630d6ba92fd0d7e7639e
sheeraz0007 <sheerazm149@gmail.com>
2022-08-26 11:26:16 +0500

    add new

---
+++ src/new.js
@@ -0,0 +1,11 @@
+const FtestingGit = () => {
+    return ( 
+        <div className="FtestingGit">
+            <div className="ttt">
+                <h1>first git test ever in life.</h1>
+            </div>
+        </div>
+     );
+}
+ 
+export default FtestingGit;
